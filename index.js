06f99ef8fd567bad1d89e04521c96395e38b4a5b
NaCoLiu <naco@nco.im>
2021-09-05 23:20:26 +0800

    update

--- index.js
+++ index.js
@@ -2,8 +2,40 @@
 
 const crypto = require("crypto")
 
-const ByteSwap = require("./byte-swap");
+const zero = 0n;
+const one = 1n;
+const n256 = 256n;
 
+class ByteSwap {
+    static from_little_endian(bytes) {
+        let result = zero;
+        let base = one;
+        bytes.forEach(function (byte) {
+            result = result + (base * (BigInt(byte)));
+            base = base * n256;
+        });
+        return result;
+    }
+
+    static from_big_endian(bytes) {
+        return this.from_little_endian(bytes.reverse());
+    }
+
+    static to_little_endian(bigNumber) {
+        let result = new Uint8Array(8);
+        let i = 0;
+        while (bigNumber > zero) {
+            result[i] = Number(bigNumber % n256);
+            bigNumber = bigNumber / n256;
+            i += 1;
+        }
+        return result;
+    }
+
+    static to_big_endian(bytes) {
+        return this.to_little_endian(bytes).reverse();
+    }
+}
 const alnum = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789"
 const ralnum = {
     'A': 0n,
